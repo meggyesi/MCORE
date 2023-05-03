15078d6f0cd03bd354b179812c1276f7160f45fa
Meggyesi Márk <h048806@stud.u-szeged.hu>
2023-05-03 15:15:16 +0200

    MCORE TORRENT SITE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,23 @@ const routes: Routes = [
     path: 'main',loadChildren: () => import('./pages/main/main.module').then(m => m.MainModule),
     canActivate: [AuthGuard]
   },
- 
+  {
+    path: 'contact',loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule)
+  },
+  {
+     path: 'download',loadChildren: () => import('./pages/download/download.module').then(m => m.DownloadModule),
+     canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'signup', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule) },
+  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) },
+  { path: 'upload', loadChildren: () => import('./pages/upload/upload.module').then(m => m.UploadModule) },
+  { path: 'table', loadChildren: () => import('./pages/table/table.module').then(m => m.TableModule) },
   {
     path: '**',
     redirectTo: '/main'
